d5c59e99154edb78bee3ed4ee9e795f887b6b722
arentas <arentas.meinorius@adeoweb.lt>
2020-07-09 17:05:14 +0300

    weathertype list in admin page

--- Block/Adminhtml/WeatherType.php
+++ Block/Adminhtml/WeatherType.php
@@ -9,7 +9,7 @@ class WeatherType extends \Magento\Backend\Block\Widget\Grid\Container
 		$this->_controller = 'adminhtml_weathertype';
 		$this->_blockGroup = 'AdeoWeb_WeatherConditions';
 		$this->_headerText = __('WeatherTypes');
-		$this->_addButtonLabel = __('fancy button');
+		$this->_addButtonLabel = __('Add new WeatherType');
 		parent::_construct();
 	}
 }
