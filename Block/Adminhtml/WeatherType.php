d305a98d4814f25e0c5614f969088c1a389c4db0
arentas <arentas.meinorius@adeoweb.lt>
2020-07-09 16:47:28 +0300

    commit for saving work

---
+++ Block/Adminhtml/WeatherType.php
@@ -0,0 +1,15 @@
+<?php
+namespace AdeoWeb\WeatherConditions\Block\Adminhtml;
+
+class WeatherType extends \Magento\Backend\Block\Widget\Grid\Container
+{
+
+	protected function _construct()
+	{
+		$this->_controller = 'adminhtml_weathertype';
+		$this->_blockGroup = 'AdeoWeb_WeatherConditions';
+		$this->_headerText = __('WeatherTypes');
+		$this->_addButtonLabel = __('fancy button');
+		parent::_construct();
+	}
+}
--- Controller/Types/Index.php
+++ Controller/Types/Index.php
@@ -4,14 +4,8 @@ class Index extends \Magento\Framework\App\Action\Action
 {
 	public function execute()
 	{
-		/*$item = $this->_objectManager->create('AdeoWeb\WeatherConditions\Model\WeatherType');
-		$item->setDescription('rainy');
-		$type = $item->getDescription();*/
-		//echo $type;
-		$list = $this->_objectManager->get('AdeoWeb\WeatherConditions\Model\WeatherTypeRepository');
-		//$item = $list->get(0);
-		var_dump($item);
-
+		$repo = $this->_objectManager->create('AdeoWeb\WeatherConditions\Model\WeatherTypeRepository');
+		$item = $this->_objectManager->create('AdeoWeb\WeatherConditions\Model\Data\WeatherType');
 
 		$type = $this->getRequest()->getParam('type');
 		$type = isset($type) ? $type : 'Sun';
